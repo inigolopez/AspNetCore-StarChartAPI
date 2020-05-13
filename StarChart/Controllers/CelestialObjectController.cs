a5170011150bbcd9fc9ad00389209d6717ad7145
Inigo Lopez <inigo.lopez@outlook.com>
2020-05-12 21:17:18 -0700

    module 5

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
     [ApiController]
-    [Route("")]
+    [Route("api/[controller]")]
     public class CelestialObjectController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
@@ -16,6 +17,53 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext applicationDbContext)
         {
             this._context = applicationDbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var objects = _context.CelestialObjects.ToList();
+
+            foreach(var o in objects)
+            {
+                o.Satellites = new List<CelestialObject>();
+                o.Satellites.AddRange(_context.CelestialObjects.Where(co => co.OrbitedObjectId == o.Id));
+            }
+
+            return Ok(objects);
+        }
+
+        [HttpGet("{id:int}",Name ="GetById")]
+        public IActionResult GetById(int id)
+        {
+            var o = _context.CelestialObjects.FirstOrDefault(co => co.Id == id);
+            
+            if (o != null)
+            {
+                if (o.Satellites == null)
+                {
+                    o.Satellites = new List<CelestialObject>();
+                    o.Satellites.AddRange(_context.CelestialObjects.Where(co => co.OrbitedObjectId == o.Id));
+                }
+                return Ok(o);
+            }
+
+            return NotFound();
+        }
+
+        [HttpGet("{name}",Name ="GetByName")]
+        public IActionResult GetByName(string name)
+        {
+            var objects = _context.CelestialObjects.Where(co => co.Name == name).ToList();
+
+            if (objects.Count == 0) return NotFound();
+
+            foreach(var o in objects)
+            {
+                o.Satellites = new List<CelestialObject>();
+                o.Satellites.AddRange(_context.CelestialObjects.Where(co => co.OrbitedObjectId == o.Id));
+            }
+            return Ok(objects);
         }
     }
 }
--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.EntityFrameworkCore;
 using StarChart.Models;
 
@@ -8,6 +10,65 @@ namespace StarChart.Data
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var sun = new CelestialObject()
+            {
+                Id = 1,
+                Name = "Sun",
+                OrbitalPeriod = new TimeSpan(0, 0, 0, 0),
+                OrbitedObjectId = 0,
+                Satellites = new List<CelestialObject>()
+            };
+
+            var earth = new CelestialObject()
+            {
+                Id = 2,
+                Name = "Earth",
+                OrbitalPeriod = new TimeSpan(365, 0, 0, 0),
+                OrbitedObjectId = 1,
+                Satellites = new List<CelestialObject>()
+            };
+
+            var moon = new CelestialObject()
+            {
+                Id = 3,
+                Name = "Moon",
+                OrbitalPeriod = new TimeSpan(28, 0, 0, 0),
+                OrbitedObjectId = 2,
+                Satellites = new List<CelestialObject>()
+            };
+
+            var jupiter = new CelestialObject()
+            {
+                Id = 4,
+                Name = "Jupiter",
+                OrbitalPeriod = new TimeSpan(876, 0, 0, 0),
+                OrbitedObjectId = 1,
+                Satellites = new List<CelestialObject>()
+            };
+
+            var calixto = new CelestialObject()
+            {
+                Id = 5,
+                Name = "Calixto",
+                OrbitalPeriod = new TimeSpan(13, 0, 0, 0),
+                OrbitedObjectId = 4,
+                Satellites = new List<CelestialObject>()
+            };
+
+            sun.Satellites.Add(earth);
+            earth.Satellites.Add(moon);
+
+            modelBuilder.Entity<CelestialObject>().HasData(moon);
+            modelBuilder.Entity<CelestialObject>().HasData(earth); 
+            modelBuilder.Entity<CelestialObject>().HasData(sun);
+            modelBuilder.Entity<CelestialObject>().HasData(jupiter);
+            modelBuilder.Entity<CelestialObject>().HasData(calixto);
         }
 
         public DbSet<CelestialObject> CelestialObjects { get; set; }
